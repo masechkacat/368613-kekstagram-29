a74c2c70322085d7f15766e483610f83eef67bd6
Marina Torunova <masechkacat@gmail.com>
2023-06-11 20:36:03 +0200

    correction

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,8 @@ isValidStr (phrase, maxSymbols);
 
 
 const isPalindrome = function (str) {
-  str = str.toLowerCase().replace(/[^а-яa-z1-9]/gi,'');
+  str = str.toLowerCase().replaceAll(' ', '');
+  //(/[^а-яa-z1-9]/gi,'');
   const lastIndex = str.length - 1;
   for (let i = 0; i < str.length / 2; i++) {
     if (str[i] !== str[lastIndex - i]) {
