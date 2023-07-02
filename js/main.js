f3521585009417e5f7a8e55a56d3c35297995b1a
Marina Torunova <masechkacat@gmail.com>
2023-07-02 14:48:06 +0200

    correction

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import { createPhotos } from './data.js';
 import { getMiniatures } from './miniatures.js';
 
-createPhotos();
-getMiniatures();
+getMiniatures(createPhotos());
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,21 +1,23 @@
-import {createPhotos} from './data.js';
+const containerPictures = document.querySelector('.pictures');
 
-const getMiniatures = () => {
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-  const containerPictures = document.querySelector('.pictures');
-  const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictureElements = ({url, description, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
 
-  const pictureElements = createPhotos();
+  return pictureElement;
+};
 
+const getMiniatures = (pictures) => {
   const containerPictureFragment = document.createDocumentFragment();
-
-  pictureElements.forEach (({url, description, likes, comments}) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__img').alt = description;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictures.forEach((picture) => {
+    const pictureElement = pictureElements(picture);
     containerPictureFragment.appendChild(pictureElement);
+
   });
 
   containerPictures.appendChild(containerPictureFragment);
