e324405cd6133c548021c0665c563f312549df12
Marina Torunova <masechkacat@gmail.com>
2023-07-17 17:41:57 +0200

    pristine

--- js/form.js
+++ js/form.js
@@ -1,8 +1,12 @@
 import { onDocumentKeydown } from './modal-window';
 
 const uploadForm = document.querySelector('.img-upload__form');
+const uploadControl = uploadForm.querySelector('.img-upload__input');
+const sendFormButton = uploadForm.querySelector('.img-upload__submit');
 const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
 const uploadCancelButton = uploadOverlay.querySelector('.img-upload__cancel');
+const hashtagField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
 
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
@@ -11,6 +15,10 @@ const pristine = new Pristine(uploadForm, {
 });
 
 const closeModal = () => {
+  if (document.activeElement === hashtagField || document.activeElement === commentField) {
+    return;
+  }
+
   uploadOverlay.classList.add('hidden');
   document.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
@@ -23,3 +31,12 @@ const openModal = () => {
   document.addEventListener('keydown', onDocumentKeydown);
   uploadCancelButton.addEventListener('click', closeModal);
 };
+
+uploadControl.addEventListener('change', () =>
+  openModal()
+);
+
+sendFormButton.addEventListener('input', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {thumbnailsList} from './data.js';
 import {renderMiniatures} from'./miniatures.js';
 import {renderBigPicture} from'./modal-window.js';
+import './form.js';
 
 renderMiniatures(thumbnailsList);
 renderBigPicture();
