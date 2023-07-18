cea328f61e2fae1a681b6b12a81b2d8f8648ede9
Marina Torunova <masechkacat@gmail.com>
2023-07-18 13:23:30 +0200

    нашла галерею

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import {thumbnailsList} from './data.js';
 import {renderMiniatures} from'./miniatures.js';
 import {renderBigPicture} from'./modal-window.js';
-import './form.js';
 
 
 renderMiniatures(thumbnailsList);
