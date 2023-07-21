680349c972208cc1f492ffae377b747f6f7d3fb3
Marina Torunova <masechkacat@gmail.com>
2023-07-21 19:15:08 +0200

    fetch

---
+++ js/api.js
@@ -0,0 +1,23 @@
+const BASE_URL = 'https://28.javascript.pages.academy/kekstagram';
+const DATA_URL = '/data';
+
+const Method = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+const load = (route, onFail, method = Method.GET, body = null) =>
+  fetch(route, {method, body})
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    })
+    .catch(() => {
+      onFail();
+    });
+
+const getData = () => load(`${BASE_URL}${DATA_URL}`, showAlert);
+
+const sendData = (body) => load(`${BASE_URL}`, showErrorMessage, Method.POST, body);
