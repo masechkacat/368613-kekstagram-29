61c184867c99cacdaf520bf8c4c992c044d7ef05
Marina Torunova <masechkacat@gmail.com>
2023-07-31 14:55:54 +0200

    new debounce

--- js/miniatures.js
+++ js/miniatures.js
@@ -2,25 +2,42 @@ const thumbnailsContainer = document.querySelector('.pictures');
 
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const thumbnailElements = ({url, description, likes, comments, id}) => {
-  const thumbnailElement = thumbnailTemplate.cloneNode(true);
-  thumbnailElement.querySelector('.picture__img').src = url;
-  thumbnailElement.querySelector('.picture__img').alt = description;
-  thumbnailElement.querySelector('.picture__likes').textContent = likes;
-  thumbnailElement.querySelector('.picture__comments').textContent = comments.length;
-  thumbnailElement.dataset.thumbnailId = id;
+const thumbnailsContainerFragment = document.createDocumentFragment();
 
-  return thumbnailElement;
+
+const createThumbnailElements = (pictures) => {
+  pictures.forEach(({url, description, likes, comments, id}) => {
+    const thumbnailElement = thumbnailTemplate.cloneNode(true);
+    thumbnailElement.querySelector('.picture__img').src = url;
+    thumbnailElement.querySelector('.picture__img').alt = description;
+    thumbnailElement.querySelector('.picture__likes').textContent = likes;
+    thumbnailElement.querySelector('.picture__comments').textContent = comments.length;
+    thumbnailElement.dataset.thumbnailId = id;
+
+    thumbnailsContainerFragment.append(thumbnailElement);
+
+    return thumbnailsContainerFragment;
+  });
 };
 
 const renderMiniatures = (pictures) => {
-  const thumbnailsContainerFragment = document.createDocumentFragment();
+  createThumbnailElements(pictures);
+  thumbnailsContainer.append(thumbnailsContainerFragment);
+};//const thumbnailsContainerFragment = document.createDocumentFragment();
+  /*const thumbnailsContainerFragment = document.createDocumentFragment();
   pictures.forEach((picture) => {
     const thumbnailElement = thumbnailElements(picture);
     thumbnailsContainerFragment.append(thumbnailElement);
 
-  });
+  };
 
   thumbnailsContainer.append(thumbnailsContainerFragment);
+};*/
+
+const removeMiniatures = () => {
+  document.querySelectorAll('.picture').forEach((picture) => {
+    picture.remove();
+  });
 };
-export {renderMiniatures};
+
+export {renderMiniatures, removeMiniatures};
